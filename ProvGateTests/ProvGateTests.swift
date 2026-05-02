cc82eb9ec91762b5765e3e0615332e924fc0eae3
joeyede <joey@deskalarm.com>
2026-05-02 22:55:11 +0100

    Initial Commit

---
+++ ProvGateTests/ProvGateTests.swift
@@ -0,0 +1,19 @@
+//
+//  ProvGateTests.swift
+//  ProvGateTests
+//
+//  Created by Joey Edelstein on 02/05/2026.
+//
+
+import Testing
+@testable import ProvGate
+
+struct ProvGateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
